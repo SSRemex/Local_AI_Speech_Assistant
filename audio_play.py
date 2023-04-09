5d875c07bbe3f2b196d3504e3b19128cab9f5ac2
SSRemex <769885884@qq.com>
2023-04-09 23:43:24 +0800

    用于声音处理

---
+++ audio_play.py
@@ -0,0 +1,37 @@
+import wave
+import pyaudio
+import subprocess
+
+
+def play(file_name):
+    # 打开WAV文件
+    with wave.open(file_name, 'rb') as wav_file:
+        # 创建PyAudio对象
+        py_audio = pyaudio.PyAudio()
+        # 打开PyAudio输出流
+        stream = py_audio.open(format=py_audio.get_format_from_width(wav_file.getsampwidth()),
+                                channels=wav_file.getnchannels(),
+                                rate=wav_file.getframerate(),
+                                output=True)
+        # 播放数据
+        data = wav_file.readframes(1024)
+        while data:
+            stream.write(data)
+            data = wav_file.readframes(1024)
+        # 关闭流
+        stream.close()
+        py_audio.terminate()
+
+
+# 调整音频播放速率
+def a_speed(input_file, speed, out_file):
+    try:
+        cmd = "ffmpeg -loglevel quiet -y -i %s -filter_complex \"atempo=tempo=%s\" %s" % (input_file, speed, out_file)
+        res = subprocess.call(cmd, shell=True)
+
+        if res != 0:
+            return False
+        return True
+    except Exception:
+        return False
+
